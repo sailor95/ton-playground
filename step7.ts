99f6f072b26976ef4895589acbb3eec66b33546b
Dave H <bugman195@gmail.com>
2023-03-08 01:53:24 +0800

    Add Orbs api

--- step7.ts
+++ step7.ts
@@ -1,6 +1,7 @@
 import { mnemonicToWalletKey } from 'ton-crypto'
-import { WalletContractV4 } from 'ton'
+import { fromNano, TonClient, WalletContractV4 } from 'ton'
 import * as dotenv from 'dotenv'
+import { getHttpEndpoint } from '@orbs-network/ton-access'
 
 dotenv.config()
 
@@ -17,6 +18,16 @@ async function main() {
 
   // Wallet workchain
   console.log('workchain:', wallet.address.workChain)
+
+  const endpoint = await getHttpEndpoint({ network: 'testnet' })
+  const client = new TonClient({ endpoint })
+
+  const balance = await client.getBalance(wallet.address)
+  console.log('Balance:', fromNano(balance))
+
+  const walletContract = client.open(wallet)
+  const seqno = await walletContract.getSeqno()
+  console.log('seqno:', seqno)
 }
 
 main()
